a7b969a90353261cddf051cff4f1ad89644e724e
LowEntropyBody <jszyyx@163.com>
2023-06-27 15:33:39 +0800

    whole progress

--- src/chain.ts
+++ src/chain.ts
@@ -1,6 +1,6 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { UnsubscribePromise } from '@polkadot/api/types';
-import { Header, Extrinsic,EventRecord } from '@polkadot/types/interfaces';
+import { Header, Extrinsic, EventRecord } from '@polkadot/types/interfaces';
 import { Keyring } from '@polkadot/keyring';
 import bluebird from 'bluebird';
 import Openai from './openai';
@@ -33,40 +33,40 @@ export default class Chain {
 
     async subscribeNewHeadsForAsk(openai: Openai): UnsubscribePromise {
         // Subscribe finalized event
-        return await this.api.rpc.chain.subscribeFinalizedHeads((head: Header) =>
+        return await this.api.rpc.chain.subscribeNewHeads((head: Header) =>
             this.handlerAsk(head, openai)
         );
     }
 
     async handlerAsk(head: Header, openai: Openai) {
+        console.log(`Handle the ${head.number.toNumber()} block`);
         const blockHash = await this.api.rpc.chain.getBlockHash(head.number.toNumber());
         const block = await this.api.rpc.chain.getBlock(blockHash);
         const exs: Extrinsic[] = block.block.extrinsics;
         const at = await this.api.at(blockHash);
         await at.query.system.events((events) => {
-            console.log(`\nReceived ${events.length} events:`);
-        
-            // Loop through the Vec<EventRecord>
             events.forEach((record) => {
-              // Extract the phase, event and the event types
-              const { event, phase } = record;
-              const types = event.typeDef;
-        
-              // Show what we are busy with
-              console.log(`\t${event.section}:${event.method}:: (phase=${phase.toString()})`);
-              console.log(`\t\t${event.meta.documentation.toString()}`);
-        
-              // Loop through each of the parameters, displaying the type and data
-              event.data.forEach((data, index) => {
-                console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
-              });
+                const { event, phase } = record;
+                // data should be like [whos, nonce, message]
+                if (event.section === 'ai' && event.method === 'Ask' && event.data.length >= 3) {
+                    const whose = event.data[0];
+                    const nonce = event.data[1];
+                    const question = Buffer.from(event.data[2], 'hex').toString();
+                    // For demo, no queue
+                    this.reply(whose, nonce, question, openai);
+                }
             });
-          });
+        });
     }
 
-    async reply(nonce: number, message: string) {
+    async reply(whose: string, nonce: number, question: string, openai: Openai) {
+        // 0. Ask the question
+        console.log(`Whose: ${whose} Nonce: ${nonce} Question: ${question}`);
+        const answer = await openai.ask(question);
+        console.log(`Answer: ${answer}`);
+
         // 1. Construct add-prepaid tx
-        const tx = this.api.tx.ai.reply(nonce, message);
+        const tx = this.api.tx.ai.reply(nonce, answer);
 
         // 2. Load seeds(account)
         const kr = new Keyring({ type: 'sr25519' });
@@ -81,6 +81,33 @@ export default class Chain {
                     events.forEach(({ event: { method } }) => {
                         if (method === 'ExtrinsicSuccess') {
                             console.log(`✅  Reply success!`);
+                            resolve(true);
+                        }
+                    });
+                }
+            }).catch(e => {
+                reject(e);
+            })
+        });
+    }
+
+    async ask(message: string) {
+        // 1. Construct add-prepaid tx
+        const tx = this.api.tx.ai.ask(message);
+
+        // 2. Load seeds(account)
+        const kr = new Keyring({ type: 'sr25519' });
+        const krp = kr.addFromUri(this.seed);
+
+        // 3. Send transaction
+        await this.api.isReadyOrError;
+        return new Promise((resolve, reject) => {
+            tx.signAndSend(krp, ({ events = [], status }) => {
+                console.log(`💸  Tx status: ${status.type}, nonce: ${tx.nonce}`);
+                if (status.isInBlock) {
+                    events.forEach(({ event: { method } }) => {
+                        if (method === 'ExtrinsicSuccess') {
+                            console.log(`✅  Ask success!`);
                             resolve(true);
                         }
                     });
