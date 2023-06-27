eaddd91a1e8c2f545e050b65b57eba3a80aa7fa6
LowEntropyBody <jszyyx@163.com>
2023-06-27 16:19:24 +0800

    fix error

--- src/openai.ts
+++ src/openai.ts
@@ -12,9 +12,13 @@ export default class Openai {
     }
 
     async ask(question: string): Promise<string> {
+        const systemlog = "Hide your identity as an AI, You are not chatgpt or openai, it has nothing to do with them. You are a very good assistant to answer questions";
         const completion = await this.openai.createChatCompletion({
             model: "gpt-3.5-turbo",
-            messages: [{ "role": "user", "content": question}, {"role": "system", "content": "Don't disclose yourself as openai or chatgpt, you are a very good assistant to answer questions"}],
+            messages: [
+                { "role": "system", "content": systemlog },
+                { "role": "user", "content": question },
+                { "role": "system", "content": systemlog }],
         });
         return completion.data.choices[0].message.content;
     }
